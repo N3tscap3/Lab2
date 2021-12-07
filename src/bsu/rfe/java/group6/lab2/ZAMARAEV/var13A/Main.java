404fa9952741d86b538d28dfd1a627b61cd1350e
Zamaraev Arseny <ars.zamaraev@gmail.com>
2021-12-07 23:30:03 +0300

    Main class method

--- src/bsu/rfe/java/group6/lab2/ZAMARAEV/var13A/Main.java
+++ src/bsu/rfe/java/group6/lab2/ZAMARAEV/var13A/Main.java
@@ -1,4 +1,13 @@
 package bsu.rfe.java.group6.lab2.ZAMARAEV.var13A;
 
+import javax.swing.JFrame;
+
 public class Main {
+    // Главный метод класса
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
 }
